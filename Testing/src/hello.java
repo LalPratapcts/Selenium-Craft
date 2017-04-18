e63b62b7aa692a4c337b349cd541be0e124e9b2f
lalpratapcts <lalpratapvit@gmail.com>
2017-04-19 01:44:47 +0530

    Change-Id: I157e8636c1d801f0145ab2872aa3cfd18ef921a5

--- Testing/src/hello.java
+++ Testing/src/hello.java
@@ -4,6 +4,7 @@ public class hello {
 	public static void main(String[] args){
 		System.out.println("hello moto");
 		System.out.println("I am learning");
+		System.out.println("I am an indian");
 		
 	}
 }
