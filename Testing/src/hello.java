de503916c1302fd505cf50c5f98078a09ba42742
lalpratapcts <lalpratapvit@gmail.com>
2017-04-19 01:33:03 +0530

    Adding new code for hello

--- Testing/src/hello.java
+++ Testing/src/hello.java
@@ -2,7 +2,8 @@
 public class hello {
 
 	public static void main(String[] args){
-		System.out.println("hello moto");	
+		System.out.println("hello moto");
+		System.out.println("I am learning");
 		
 	}
 }
